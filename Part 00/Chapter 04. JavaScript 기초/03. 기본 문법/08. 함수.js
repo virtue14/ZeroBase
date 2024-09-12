da1ab5c4b337f4e6b63e88d3038564b83965e132
악덕 <rudejrqkr97@gmail.com>
2024-09-13 00:37:23 +0900

    learn(JavaScript): 함수

---
+++ Part 00/Chapter 04. JavaScript 기초/03. 기본 문법/08. 함수.js
@@ -0,0 +1,27 @@
+/**
+ * 함수
+ */
+
+// 함수 선언문 (매개변수 X)
+function func() {
+	if (10 > 0) {
+		return 'Hello';
+	}
+
+	return undefined;
+}
+
+// 함수 선언문 (매개변수 O)
+function sum(num1, num2, num3, num4) {
+	return num1 + num4;
+}
+
+// 함수 선언문 (매개변수 O)
+function multiple(num1, num2) {
+	return num1 * num2;
+}
+
+// 반환 없는 함수
+function voidFunc() {
+	console.log('void func');
+}
