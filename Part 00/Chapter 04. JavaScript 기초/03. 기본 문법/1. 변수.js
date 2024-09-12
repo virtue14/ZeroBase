d6d18313d6263b76c8b646f7abed8d6dcad71117
악덕 <rudejrqkr97@gmail.com>
2024-09-12 22:55:28 +0900

    learn(JavaScript): 변수

---
+++ Part 00/Chapter 04. JavaScript 기초/03. 기본 문법/1. 변수.js
@@ -0,0 +1,29 @@
+/**
+ * - 추천
+ * 선언과 동시에 할당
+ */
+
+const lang = 'JavaScript';
+
+// 선언
+let lang2;
+
+// 할당
+lang2 = 'JS';
+
+// 재할당
+lang2 = 'JS';
+
+// 선언과 동시에 할당
+let lang3 = 'JavaScript';
+
+// 재할당
+lang3 = 'JS';
+
+let count = 0;
+
+// 복합 할당 연산자 (예측하기 어려움)
+count += 1;
+
+// 잘 읽히고 예상하기 쉬움
+count = count + 1;
