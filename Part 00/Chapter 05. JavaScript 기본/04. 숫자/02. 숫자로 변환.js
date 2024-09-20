2920dabd4724f8c12ab6634b233edf42f7020946
악덕 <rudejrqkr97@gmail.com>
2024-09-21 08:16:54 +0900

    learn(JavaScript): 숫자로 변환

---
+++ Part 00/Chapter 05. JavaScript 기본/04. 숫자/02. 숫자로 변환.js
@@ -0,0 +1,20 @@
+console.log(undefined);
+console.log(null);
+console.log('');
+console.log('String');
+console.log(true);
+console.log(false);
+console.log(3.141592);
+
+if (1 == true) {
+	console.log('참');
+}
+
+console.log(+true);
+console.log(+false);
+
+const strNumber = '11111';
+
+if (Number(strNumber)) {
+	console.log('실행');
+}
