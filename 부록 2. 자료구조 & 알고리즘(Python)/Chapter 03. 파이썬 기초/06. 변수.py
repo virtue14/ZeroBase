0ff8cf1eca523615f4055991157716e44fdfe450
악덕 <rudejrqkr97@gmail.com>
2024-09-27 19:17:46 +0900

    learn(Python): 변수

---
+++ 부록 2. 자료구조 & 알고리즘(Python)/Chapter 03. 파이썬 기초/06. 변수.py
@@ -0,0 +1,15 @@
+number = 20
+
+print(number)
+
+name = 'Hong Gil Dong'
+age = 21
+address = 'Seoul'
+phone = '010-1234-5678'
+mail_addr = 'hong@gmail.com'
+
+print(name)
+print(age)
+print(address)
+print(phone)
+print(mail_addr)
