64607f8d23db36e8f39bbdf250cc458815f6a8ea
악덕 <rudejrqkr97@gmail.com>
2024-09-29 18:11:46 +0900

    learn(Python): 변수를 사용하는 이유

---
+++ 부록 2. 자료구조 & 알고리즘(Python)/Chapter 03. 파이썬 기초/07. 변수를 사용하는 이유.py
@@ -0,0 +1,22 @@
+print('Hello')
+
+intro = 'Hello'
+print(intro)
+
+num1 = 10
+num2 = 20
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+name = '홍길동'
+print('고객님께')
+print('고객님 안녕하세요.')
+print('고객님꼐서 접수하신 A/S건에 대해서 연락을 드렸으나 연락이 어려워 메일 드립니다.')
+print('A/S 접수 내용')
+print('----------')
+print('성함: ', name)
+print('내용: 에어컨 고장')
+print('----------')
