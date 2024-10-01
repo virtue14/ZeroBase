9352b9e4f6a1939dc1f1b83e5034a43e92b43e5e
악덕 <rudejrqkr97@gmail.com>
2024-10-01 18:55:30 +0900

    learn(Python): 자료형 변환(문자)

---
+++ 부록 2. 자료구조 & 알고리즘(Python)/Chapter 03. 파이썬 기초/10. 자료형 변환(문자).py
@@ -0,0 +1,33 @@
+iNum = 10
+fNum = 3.14
+
+print(iNum)
+print(type(iNum))
+
+print(fNum)
+print(type(fNum))
+
+iNum = str(iNum)
+print(str(iNum))
+print(type(iNum))
+
+fNum = str(fNum)
+print(fNum)
+print(type(fNum))
+
+flag = True
+print(flag)
+print(type(flag))
+
+flag = str(True)
+print(flag)
+print(type(flag))
+
+fNum1 = 3.14
+fNum2 = 0.123
+
+print(fNum1 + fNum2)
+
+fNum1 = str(fNum1)
+fNum2 = str(fNum2)
+print(fNum1 + fNum2)
