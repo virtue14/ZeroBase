97f8dd51743528a8f20f55551f5643552d5ac155
악덕 <rudejrqkr97@gmail.com>
2024-10-01 18:57:21 +0900

    learn(Python): 자료형 변환(숫자)

---
+++ 부록 2. 자료구조 & 알고리즘(Python)/Chapter 03. 파이썬 기초/11. 자료형 변환(숫자).py
@@ -0,0 +1,39 @@
+var = '100'
+print(var)
+print(type(var))
+
+var = int(var)
+print(var)
+print(type(var))
+
+var = float(var)
+print(var)
+print(type(var))
+
+var = True
+print(var)
+print(type(var))
+
+var = int(var)
+print(var)
+print(type(var))
+
+var = float(var)
+print(var)
+print(type(var))
+
+str1 = '10'
+str2 = '20'
+print(str1 + str2)
+
+str1 = int(str1)
+str2 = int(str2)
+print(str1 + str2)
+
+print(float(str1 + str2))
+
+str1 = '3.14'
+str2 = '1592'
+print(str1 + str2)
+
+print(float(str1) + int(str2))
