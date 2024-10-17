ec5e3050caafe1602dff1370df6954b4c0564379
악덕 <rudejrqkr97@gmail.com>
2024-10-18 01:59:53 +0900

    learn(Java): 람다식

---
+++ Part 01. Java 기초/Chapter 01. Java 프로그래밍/Java_16_람다식/Main.java
@@ -0,0 +1,33 @@
+// Java 프로그래밍 - 람다식
+package Java_16_람다식;
+
+interface ComputeTool {
+    public abstract int compute(int x, int y);
+
+    public abstract int compute2(int x, int y);
+}
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        ComputeTool cTool1 = new ComputeTool() {
+            @Override
+            public int compute(int x, int y) {
+                return x + y;
+            }
+
+            @Override
+            public int compute2(int x, int y) {
+                return x - y;
+            }
+        };
+        System.out.println("cTool1 = " + cTool1.compute(1, 2));
+
+        // 람다식
+//        ComputeTool cTool2 = (x, y) -> { return x + y; };
+//        System.out.println("cTool2 = " + cTool2.compute(1, 2));
+
+    }
+}
