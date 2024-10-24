68642213da89e7d42b60d804e4ce6ac1a60417ce
악덕 <rudejrqkr97@gmail.com>
2024-10-24 22:53:27 +0900

    learn(Java): 연습 문제 풀이 - 로마 숫자에서 정수로 변환

---
+++ Part 01. Java 기초/Chapter 01. Java 프로그래밍/Java_18_연습문제풀이2/Practice1.java
@@ -0,0 +1,39 @@
+package Java_18_연습문제풀이2;
+
+import java.util.HashMap;
+
+public class Practice1 {
+    public static void solution(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int sum = 0;
+        char[] arr = s.toCharArray();
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (map.get(arr[i]) < map.get(arr[i + 1])) {
+                sum -= map.get(arr[i]);
+            } else {
+                sum += map.get(arr[i]);
+            }
+        }
+        sum += map.get(arr[arr.length - 1]);
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+        // Test code
+        solution("III");
+        solution("IV");
+        solution("VI");
+        solution("XIII");
+        solution("XXVI");
+        solution("MCMXCIV");
+    }
+}
