3829127efc9458ccbe44d24bd15f3565b6efb7a9
악덕 <rudejrqkr97@gmail.com>
2024-10-24 22:44:19 +0900

    learn(Java): 연습 문제 풀이 - 정수 뒤집기

---
+++ Part 01. Java 기초/Chapter 01. Java 프로그래밍/Java_18_연습문제풀이1/Practice1.java
@@ -0,0 +1,28 @@
+package Java_18_연습문제풀이1;
+
+public class Practice1 {
+    public static void solution(int num) {
+        int numReverse = 0;
+        boolean isMinus = false;
+
+        if (num < 0) {
+            isMinus = true;
+            num *= -1;
+        }
+
+        while (num > 0) {
+            int r = num % 10;
+            num /= 10;
+            numReverse = numReverse * 10 + r;
+        }
+        System.out.println(isMinus ? numReverse * -1 : numReverse);
+    }
+
+    public static void main(String[] args) {
+        // Test code
+        solution(12345);
+        solution(-12345);
+        solution(100);
+        solution(0);
+    }
+}
